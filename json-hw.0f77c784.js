c8618f717db7d3a1965118c6baa8027331db9641
Mykola Sadlii <mykolac08@gmail.com>
2026-04-16 19:55:42 +0100

    Updates

--- json-hw.0f77c784.js
+++ json-hw.0f77c784.js
@@ -718,6 +718,7 @@ const addBtnRef = document.getElementById("addBookmarkBtn");
 const bookmarksListRef = document.querySelector("#bookmarkList");
 const inputRef = document.querySelector("#bookmarkInput");
 let arrayOfBookmarks = [];
+const STORAGE_KEY = "listOfBookmarks";
 function createItems(array) {
     const items = array.map((item)=>{
         return `<li>
@@ -726,21 +727,22 @@ function createItems(array) {
 </li>`;
     }).join("");
     bookmarksListRef.innerHTML = items;
-    bookmarksListRef.addEventListener("click", (e)=>{
-        if (e.target.classList.contains("delete")) {
-            const li = e.target.parentElement;
-            const value = li.querySelector("a").textContent;
-            arrayOfBookmarks = arrayOfBookmarks.filter((item)=>item !== value);
-            li.remove();
-        }
-    });
 }
 addBtnRef.addEventListener("click", ()=>{
     arrayOfBookmarks.push(inputRef.value);
     inputRef.value = "";
     createItems(arrayOfBookmarks);
-}) // 2
-;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(arrayOfBookmarks));
+});
+bookmarksListRef.addEventListener("click", (e)=>{
+    if (e.target.classList.contains("delete")) {
+        const li = e.target.parentElement;
+        const value = li.querySelector("a").textContent;
+        arrayOfBookmarks = arrayOfBookmarks.filter((item)=>item !== value);
+        li.remove();
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(arrayOfBookmarks));
+    }
+});
 
 },{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequired126", {})
 

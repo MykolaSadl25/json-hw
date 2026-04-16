85a01ac51008f2a5296f11f47942e1b53264f115
Mykola Sadlii <mykolac08@gmail.com>
2026-04-16 19:55:55 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const bookmarksListRef = document.querySelector("#bookmarkList");
 const inputRef = document.querySelector("#bookmarkInput");
 let arrayOfBookmarks = [];
 
+const STORAGE_KEY = "listOfBookmarks"
+
 function createItems(array) {
     const items = array.map(item=>{
         return `<li>
@@ -11,22 +13,24 @@ function createItems(array) {
 </li>`
     }).join("");
     bookmarksListRef.innerHTML = items;
-    bookmarksListRef.addEventListener("click",(e)=>{
-        if (e.target.classList.contains("delete")) {
-        const li = e.target.parentElement;
-        const value = li.querySelector("a").textContent;
-        arrayOfBookmarks = arrayOfBookmarks.filter(item => item !== value);
-        li.remove();         
-        }
-    })
 }
 addBtnRef.addEventListener("click",()=>{
     arrayOfBookmarks.push(inputRef.value)
     inputRef.value = "";
     createItems(arrayOfBookmarks)
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(arrayOfBookmarks)) 
 })
 
-// 2
+bookmarksListRef.addEventListener("click",(e)=>{
+        if (e.target.classList.contains("delete")) {
+        const li = e.target.parentElement;
+        const value = li.querySelector("a").textContent;
+        arrayOfBookmarks = arrayOfBookmarks.filter(item => item !== value);
+        li.remove();
+        localStorage.setItem(STORAGE_KEY,JSON.stringify(arrayOfBookmarks))       
+        }
+    })
+
 
 
 
